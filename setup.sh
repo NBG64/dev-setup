5dea36a3aaf04e87f244234b7f03b71e227350a4
NBG64 <nbaylis64@gmail.com>
2025-03-08 11:30:25 -0500

    built main setup script, added other scripts that will run

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "hello world"
+
+#setup package manager variable
+
+case $1 in
+    "pacman")
+        install_method="pacman -S"
+        ;;
+    "apt")
+        install_method="apt install"
+        ;;
+esac
+
+case $2 in 
+    "sway")
+        tiling_wm="sway"
+        ;;
+    "i3")
+        tiling_wm="i3"
+        ;;
+    "")
+        tiling_wm=""
+        ;;
+esac
+
+$install_method gcc clang unzip zip vim cmake make ripgrep git npm $tiling_wm
+if [$tiling_wm != ""]; then
+    source wm.sh
+fi
+source lua.sh
+source python.sh
+source nvim.sh
+source angular.sh
